61a969c7c5c7536d9fcda90d03e4cf25abc27164
Błażej Lewandowski <blazej.lew95@gmail.com>
2021-07-21 15:35:46 +0200

    Add base file tree structure and path aliases

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@hoc': './src/hoc',
+          '@hooks': './src/hooks',
+          '@localization': './src/localization',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
