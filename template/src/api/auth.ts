5686f38bcc9832c9408bb760fb9236cbe073f83f
Błażej Lewandowski <blazej.lew95@gmail.com>
2023-06-19 17:38:33 +0200

    replace eslint-disable line with variable prefixed with underscore for visibility

--- template/src/api/auth.ts
+++ template/src/api/auth.ts
@@ -2,16 +2,15 @@ import { AuthTokens } from './common'
 import { Credentials } from './types/auth.types'
 
 // This is the place where you want to call your auth provider API
-/* eslint-disable @typescript-eslint/no-unused-vars */
 
-export const logIn = (credentials: Credentials) => {
+export const logIn = (_credentials: Credentials) => {
   return new Promise<{ accessToken: string; refreshToken: string }>(resolve => {
     const fakeAuthTokens = { accessToken: 'FAKE_ACCESS_TOKEN', refreshToken: 'FAKE_REFRESH_TOKEN' }
     setTimeout(() => resolve(fakeAuthTokens), 500)
   })
 }
 
-export const refreshTokens = (refreshToken: AuthTokens['refreshToken']) => {
+export const refreshTokens = (_refreshToken: AuthTokens['refreshToken']) => {
   return new Promise<{ accessToken: string; refreshToken: string }>(resolve => {
     const fakeRefreshedAuthTokens = {
       accessToken: 'REFRESHED_ACCESS_TOKEN',
